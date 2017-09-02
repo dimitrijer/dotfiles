7beeedfd43f3ced6c6e76a79c6bef6608d647cca
Dimitrije Radojević <dimitrijer@nordeus.eu>
2017-09-02 09:53:41 +0200

    Update lein plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,10 @@
-{:user {:plugins [[cider/cider-nrepl  "0.13.0"]
+{:user {:plugins [[cider/cider-nrepl  "0.15.0"]
                   [venantius/ultra "0.5.1"]
-                  [jonase/eastwood "0.2.3"]
+                  [jonase/eastwood "0.2.4"]
                   [lein-ancient "0.6.10"]
                   [lein-kibit "0.1.5"]
                   [lein-bikeshed "0.4.1"]]
-        :dependencies [[pjstadig/humane-test-output  "0.8.1"]
+        :dependencies [[pjstadig/humane-test-output  "0.8.2"]
                        [slamhound "1.5.5"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
