69366287786d715acee449364d4ea6173de6acc0
Dimitrije Radojević <dimitrijer@nordeus.eu>
2018-12-20 11:15:58 +0100

    Update lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,15 +1,15 @@
-{:user {:plugins [[cider/cider-nrepl  "0.15.0"]
-                  [venantius/ultra "0.5.1"]
-                  [jonase/eastwood "0.2.4"]
-                  [lein-ancient "0.6.10"]
-                  [lein-kibit "0.1.5"]
-                  [lein-bikeshed "0.4.1"]]
-        :dependencies [[pjstadig/humane-test-output  "0.8.2"]
+{:user {:plugins [[cider/cider-nrepl  "0.18.0"]
+                  [venantius/ultra "0.5.2"]
+                  [jonase/eastwood "0.3.4"]
+                  [lein-ancient "0.6.15"]
+                  [lein-kibit "0.1.6"]
+                  [lein-bikeshed "0.5.1"]]
+        :dependencies [[pjstadig/humane-test-output  "0.9.0"]
                        [slamhound "1.5.5"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :mirrors {#".+" {:name "nordeus-nexus"
-                         :url "https://nexus.hq.nordeus.com/content/groups/public/"}}}
  :auth {:repository-auth {#"nexus\.hq\.nordeus\.com" {:username "#######"
                                                       :password "#######" }}}}
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
+ :nordeus {:mirrors {#".+" {:name "nordeus-nexus"
+                            :url "https://nexus.hq.nordeus.com/content/groups/public/"}}}
